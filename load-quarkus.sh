af037b5babddef8b6b21e28881aafc79596e9734
erkerc <erkan.ercan@gmail.com>
2023-01-20 13:13:38 +0300

    updated load-quarkus.sh

--- load-quarkus.sh
+++ load-quarkus.sh
@@ -1,4 +1,4 @@
-export SVC_PRIMEGEN_URL=`oc get rt quarkus-prime -n serverless-demo -o yaml | yq read - 'status.url'` 
+export SVC_PRIMEGEN_URL=`oc get rt quarkus-prime -n serverless-demo| awk '(NR>1)'|awk '{print $2}'` 
 hey -c 100 -z 30s \
   -H "Host:prime-generator.knativetutorial.example.com" \
   "${SVC_PRIMEGEN_URL}/?sleep=3&upto=10000&memload=100"
